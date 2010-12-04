d3f894116194c7b2614cd6f345d2953db670ce85
Dmitry A. Ustalov <dmitry@eveel.ru>
2010-12-04 19:13:38 +0500

    string_strategy: Integer support

--- lib/shkuph/strategies/string_strategy.rb
+++ lib/shkuph/strategies/string_strategy.rb
@@ -3,6 +3,7 @@
 class Shkuph::StringStrategy < Shkuph::Strategy
   def dump(value)
     return '' unless value
+    value = value.to_s if value.is_a? Integer
     raise TypeError unless value.is_a? String
     value
   end
